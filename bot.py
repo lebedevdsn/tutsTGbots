29d2fdbe00869a3ea8c7b3f6b99252e5e85d5dc7
Arseniy Lebedev <lebedevdsn@gmail.com>
2024-12-07 13:46:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,46 @@
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from aiogram.utils import executor
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram import F
+from aiogram.filters import Command
+from aiogram.webhook.aiohttp import WebhookResponse
 
-# Токен от BotFather
-API_TOKEN = "7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8"  # Замени на свой токен
+API_TOKEN = "7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8"
 
-# Инициализация бота
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 # Обработчик команды /start
-@dp.message_handler(commands=["start"])
-async def send_welcome(message: types.Message):
-    # Создание кнопки с Mini App
-    web_app = WebAppInfo(url="https://lebedevdsn.github.io/tutsTGbots")  # Замени на свой URL
-    keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton("Открыть Mini App", web_app=web_app))
-    
-    # Отправка сообщения с кнопкой
-    await message.answer("Привет! Нажми на кнопку, чтобы открыть Mini App:", reply_markup=keyboard)
+@dp.message(Command("start"))
+async def cmd_start(message: Message):
+    await message.answer("Привет! Нажми на кнопку, чтобы открыть Mini App:")
 
-# Запуск бота
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    dp.run_polling()
+
+
+
+
+# from aiogram import Bot, Dispatcher, types
+# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+# from aiogram.utils import executor
+
+# # Токен от BotFather
+# API_TOKEN = "7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8"  # Замени на свой токен
+
+# # Инициализация бота
+# bot = Bot(token=API_TOKEN)
+# dp = Dispatcher(bot)
+
+# # Обработчик команды /start
+# @dp.message_handler(commands=["start"])
+# async def send_welcome(message: types.Message):
+#     # Создание кнопки с Mini App
+#     web_app = WebAppInfo(url="https://lebedevdsn.github.io/tutsTGbots")  # Замени на свой URL
+#     keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton("Открыть Mini App", web_app=web_app))
+    
+#     # Отправка сообщения с кнопкой
+#     await message.answer("Привет! Нажми на кнопку, чтобы открыть Mini App:", reply_markup=keyboard)
+
+# # Запуск бота
+# if __name__ == "__main__":
+#     executor.start_polling(dp, skip_updates=True)
