aa51dc4f30faf6542e1609dc9c43f3fb8b307f96
Arseniy Lebedev <lebedevdsn@gmail.com>
2024-12-07 14:00:02 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,25 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
 
-# Функция, которая будет отправлять сообщение "Привет мир"
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Привет мир')
+# Функция для команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("Открой мини приложение", web_app={"url": "https://ТВОЙ_СЕРВЕР/mini_app.html"})],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    await update.message.reply_text('Добро пожаловать! Нажмите на кнопку, чтобы открыть мини-приложение:', reply_markup=reply_markup)
 
 def main():
-    # Вставь сюда свой токен
     token = '7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8'
-    
-    # Создаем объект Updater и передаем токен
-    updater = Updater(token)
 
-    # Получаем диспетчер для обработки команд
-    dispatcher = updater.dispatcher
+    # Создаем объект Application и передаем токен
+    application = Application.builder().token(token).build()
 
     # Обработчик команды /start
-    dispatcher.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
     # Запускаем бота
-    updater.start_polling()
-
-    # Бот будет работать до принудительной остановки
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
