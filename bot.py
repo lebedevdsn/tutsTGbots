3f6ead65a1885c5c9bc480be3c500db2c91faa21
Arseniy Lebedev <lebedevdsn@gmail.com>
2024-12-07 13:49:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,46 +1,28 @@
-from aiogram import Bot, Dispatcher
-from aiogram.types import Message
-from aiogram import F
-from aiogram.filters import Command
-from aiogram.webhook.aiohttp import WebhookResponse
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 
-API_TOKEN = "7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8"
+# Функция, которая будет отправлять сообщение "Привет мир"
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('Привет мир')
 
-bot = Bot(token=API_TOKEN)
-dp = Dispatcher()
-
-# Обработчик команды /start
-@dp.message(Command("start"))
-async def cmd_start(message: Message):
-    await message.answer("Привет! Нажми на кнопку, чтобы открыть Mini App:")
-
-if __name__ == "__main__":
-    dp.run_polling()
-
-
-
-
-# from aiogram import Bot, Dispatcher, types
-# from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-# from aiogram.utils import executor
-
-# # Токен от BotFather
-# API_TOKEN = "7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8"  # Замени на свой токен
-
-# # Инициализация бота
-# bot = Bot(token=API_TOKEN)
-# dp = Dispatcher(bot)
-
-# # Обработчик команды /start
-# @dp.message_handler(commands=["start"])
-# async def send_welcome(message: types.Message):
-#     # Создание кнопки с Mini App
-#     web_app = WebAppInfo(url="https://lebedevdsn.github.io/tutsTGbots")  # Замени на свой URL
-#     keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton("Открыть Mini App", web_app=web_app))
+def main():
+    # Вставь сюда свой токен
+    token = '"7758221545:AAF5qzVWzBqB_eqIitAlADFR3_di2jBFGC8"
     
-#     # Отправка сообщения с кнопкой
-#     await message.answer("Привет! Нажми на кнопку, чтобы открыть Mini App:", reply_markup=keyboard)
+    # Создаем объект Updater и передаем токен
+    updater = Updater(token)
 
-# # Запуск бота
-# if __name__ == "__main__":
-#     executor.start_polling(dp, skip_updates=True)
+    # Получаем диспетчер для обработки команд
+    dispatcher = updater.dispatcher
+
+    # Обработчик команды /start
+    dispatcher.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    updater.start_polling()
+
+    # Бот будет работать до принудительной остановки
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
